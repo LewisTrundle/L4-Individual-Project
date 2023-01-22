491e2ec65691846e3cbf5ebdeb534391cc78cbc9
LewisTrundle <lewis.trundle.1510@gmail.com>
2023-01-22 13:52:36 +0000

    remove build folder

--- src/build/src/components.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const indexPage: () => void;
-export declare var joyZone: any;
-export declare const joystickPage: () => void;
--- src/build/src/robot.d.ts
+++
@@ -1,3 +0,0 @@
-import { DeviceController } from "@espruino-tools/core";
-export declare class Robot extends DeviceController {
-}
